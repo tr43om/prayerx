19c0f85d02ee6be1f8ac9a0f0e03749c8ee1c318
tr43om <tr43om@gmail.com>
2022-09-16 03:48:24 +0600

    Added api service for signIn and signOut

--- src/api/auth/index.ts
+++ src/api/auth/index.ts
@@ -1,1 +1,33 @@
+import {api} from '../api';
+import {ACCESS_TOKEN} from '../../constants';
+import {AuthSignInDto, AuthSignUpDto} from '../../types';
 
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export const signIn = async ({email, password}: AuthSignInDto) => {
+  try {
+    const {
+      data: {token},
+    } = await api.post('auth/sign-in', {email, password});
+
+    if (token) {
+      await AsyncStorage.setItem(ACCESS_TOKEN, token);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const signUp = async ({email, name, password}: AuthSignUpDto) => {
+  try {
+    const {
+      data: {token},
+    } = await api.post('auth/sign-up', {email, name, password});
+
+    if (token) {
+      await AsyncStorage.setItem(ACCESS_TOKEN, token);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
